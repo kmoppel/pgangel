4000af851409b9b5620f0ddbcd10098a9564b05a
kmoppel <kaarel.moppel@zalando.de>
2014-06-10 14:49:49 +0200

    IO module for reading password files etc added

---
+++ Pgangel.IO/ConfigReader.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Pgangel.IO
+{
+	public class ConfigReader
+	{
+		public ConfigReader ()
+		{
+		}
+
+		public static string GetPasswordFromPasswordsFile(string hostname, string username)
+		{
+			return "pass";
+		}
+	}
+}
