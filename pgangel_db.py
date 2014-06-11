84eda9d4ffb739446f5a2866a1934e8ab9b9a174
kmoppel <kaarel.moppel@zalando.de>
2014-06-11 14:40:52 +0200

    playng around with glade

--- pgangel_db.py
+++ pgangel_db.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import psycopg2
+
 class DbServer():
     def __init__(self):
         self.name = None
@@ -11,7 +13,29 @@ class DbServer():
         self.password = None
 
 
-def DBConnection(object):
+class DBConnection():
+
+    def __init__(self, host, port, dbname, user, password=None):
+        self.host = host
+        self.port = port
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.connection = None
+
+    def try_connect(self):
+        try:
+            conn = psycopg2.connect(host=self.host, port=self.port, user=self.user)
+            conn.close()
+            return True
+        except Exception as e:
+            print e
+            return False
 
     def execute_query():
         return [{'a' : 1}]
+
+
+if __name__ == '__main__':
+    dbc = DBConnection('localhost', 5432, 'postgres', 'kmoppel')
+    print dbc.try_connect()
--- pgangel_gui.py
+++ pgangel_gui.py
@@ -3,32 +3,82 @@
 
 from gi.repository import Gtk
 
-class NewServer(Gtk.Dialog):
-    def __init__(self, parent):
-        Gtk.Dialog.__init__(self, "Create a new server", parent, 0,
-            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-             Gtk.STOCK_OK, Gtk.ResponseType.OK))
+import pgangel_conf
+import pgangel_db
 
-        label = Gtk.Label("This is a dialog to display additional information")
+ns = None # 'new server' dialog handle
 
-        box = self.get_content_area()
-        box.add(label)
-        self.set_default_size(500, 400)
+class ServerDialogHandler:
+    def on_delete_window(self, *args):
+        # print args
+        args[0].destroy()
+        #Gtk.main_quit(args)
 
-        self.entry = Gtk.Entry()
-        self.entry.set_text("localhost")
-        box.pack_end(self.entry, True, True, 0)
-        # self.show_all()
+    def on_button_test_connection_clicked(self, *args):
+        dbc = pgangel_db.DBConnection()
 
+    def on_button_ok_clicked(self, *args):
+        print "ok"
+
+    def on_button_cancel_clicked(self, *args):
+        print "cancel"
+
+class ServerDialog():
+    def __init__(self):
+        self.dialog = None
+        self.entry_name = None
+        self.entry_host = None
+        self.entry_port = None
+        self.entry_db = None
+        self.entry_user = None
+        self.entry_password = None
+
+    def build(self):
+        builder = Gtk.Builder()
+        ''':type : gtk.Builder'''
+        builder.add_from_file("resources/newserver1.xml")
+        builder.connect_signals(self)
+
+        # print builder.get_objects()
+        self.dialog = builder.get_object("dialog1")
+        ''':type : gtk.Dialog'''
+        self.entry_port = builder.get_object("entry_port")
+
+
+        # notebook1 = Gtk.Notebook()
+        # textview1 = create_text()
+        # textview2 = create_text()
+        # notebook1.append_page(textview1, None)
+        # notebook1.append_page(textview2, None)
+        #
+        # paned1.add1(notebook1)
+        # textview3 = create_text()
+        # paned1.add2(textview3)
+
+
+    def on_delete_window(self, *args):
+        print self.entry_port.get_text()
+        args[0].destroy()
+        #Gtk.main_quit(args)
+
+    def on_button_test_connection_clicked(self, *args):
+        dbc = pgangel_db.DBConnection()
+
+    def on_button_ok_clicked(self, *args):
+        print "ok"
+
+    def on_button_cancel_clicked(self, *args):
+        print "cancel"
 
 if __name__ == '__main__':
     win = Gtk.Window(title='Testing')
     win.connect('delete-event', Gtk.main_quit)
-
-    ns = NewServer(win)
-    response = ns.run()
-    ns.destroy()
+    ns = ServerDialog()
+    ns.build()
+    # ns.dialog.set_parent(win)
+    response = ns.dialog.run()
+    # ns.destroy()
     win.show_all()
     Gtk.main()
-    print response
+    # print response
 
