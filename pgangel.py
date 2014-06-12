7bb7a0c628413515a2063f10d44d0101dc715694
kmoppel <kaarel.moppel@zalando.de>
2014-06-12 10:53:04 +0200

    corrections

--- pgangel.py
+++ pgangel.py
@@ -5,57 +5,43 @@ from gi.repository import Gtk
 import pgangel_gui
 import pgangel_conf
 
-class Pgangel(Gtk.Window):
+class Pgangel():
 
     def __init__(self):
-        Gtk.Window.__init__(self, title='PgAngel')
-        self.set_default_size(400, 200)
-        grid = Gtk.Grid()
-        self.add(grid)
+        self.window = None
+        ''':type : gtk.pgadow'''
+        self.toolbutton1 = None
+        ''':type : gtk.ToolButton'''
+        self.db_servers = None
+        # ''':type : list(pgangel_db.DbServer)'''
+        # self.db_servers[0].
 
-        #uimanager = Gtk.UIManager()
-        # uimanager.add_ui_from_file('mainmenu.xml')
-        #accelgroup = uimanager.get_accel_group()
-        #self.add_accel_group(accelgroup)
-        button1 = Gtk.Button(label="Button 1")
-        button2 = Gtk.Button(label="Button 2")
-        button3 = Gtk.Button(label="Button 3")
+    def build(self):
+        builder = Gtk.Builder()
+        ''':type : gtk.Builder'''
+        builder.add_from_file("resources/main1.xml")
+        builder.connect_signals(self)
 
-        mb = Gtk.MenuBar()
-        filemenu = Gtk.Menu()
-        filem = Gtk.MenuItem("File")
-        filem.set_submenu(filemenu)
-
-        exit = Gtk.MenuItem("Exit")
-        exit.connect("activate", Gtk.main_quit)
-        filemenu.append(exit)
-
-        agr = Gtk.AccelGroup()
-        self.add_accel_group(agr)
-        key, mod = Gtk.accelerator_parse("<Control>Q")
-        exit.add_accelerator("activate", agr, key, mod, Gtk.AccelFlags.VISIBLE)
-
-        mb.append(filem)
-
-        grid.add(mb)
-        grid.add(button1)
-        grid.attach(button2, 1, 0, 2, 1)
-        grid.attach_next_to(button3, button1, Gtk.PositionType.BOTTOM, 1, 2)
+        # print builder.get_objects()
+        self.dialog = builder.get_object("dialog1")
+        ''':type : gtk.Dialog'''
+        self.entry_name = builder.get_object("entry_name")
         self.connect('delete-event', Gtk.main_quit)
 
 
-    def on_button1_clicked(self, widget):
-        print 'Hello'
+    def on_delete_pgadow(self, *args):
+        Gtk.main_quit(args)
 
 
 if __name__ == '__main__':
     pgangel_conf.ensure_app_folder_and_configs()
 
-    win = Pgangel()
-    current_servers = pgangel_conf.get_saved_servers()
-    if len(current_servers) == 0:   # offer to create a server first
-        ns = pgangel_gui.NewServer(win)
+    pga = Pgangel()
+    pga.db_servers = pgangel_conf.get_saved_servers()
+    if len(pga.db_servers) == 0:   # offer to create a server first
+        ns = pgangel_gui.NewServer(pga)
         ns.run()
         ns.destroy()
-    win.show_all()
+
+    pga.window.show_all()
     Gtk.main()
--- pgangel_db.py
+++ pgangel_db.py
@@ -8,27 +8,26 @@ import psycopg2.extras
 import json
 
 class DBConnection():
-2
-    def __init__(self, host, database, port='5432', username=None, password=None):
-        if not (username and password):
-            pgpass = os.path.expanduser('~/.pgpass')
-            auths = [re.sub('#.*$|\n', '', line) for line in open( pgpass, r'r') if not re.match('#.*', line.strip())]
-            auths = [line.replace('*', '.*') for line in auths]   #pgpass *'s are converted to .*'s
-            for auth in auths:
-                auth_parts = auth.split(':')    #host:port:database:user:pass
-                if re.match(auth_parts[0], host) and re.match(auth_parts[1], port) and re.match(auth_parts[2], database):
-                    username = username or auth_parts[3]
-                    password = password or auth_parts[4]
-        db_conn = psycopg2.connect(host=self.host, port=self.port, username=self.username,password=self.password)
-        db_conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+    def __init__(self, host, port, database, username=None, password=None):
+        # if not (username and password):
+        #     pgpass = os.path.expanduser('~/.pgpass')
+        #     auths = [re.sub('#.*$|\n', '', line) for line in open( pgpass, r'r') if not re.match('#.*', line.strip())]
+        #     auths = [line.replace('*', '.*') for line in auths]   #pgpass *'s are converted to .*'s
+        #     for auth in auths:
+        #         auth_parts = auth.split(':')    #host:port:database:user:pass
+        #         if re.match(auth_parts[0], host) and re.match(auth_parts[1], port) and re.match(auth_parts[2], database):
+        #             username = username or auth_parts[3]
+        #             password = password or auth_parts[4]
         self.host = host
         self.port = port
         self.database = database
         self.username = username
         self.password = password
-        self.connection = db_conn
-        self.cursor = db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         self.dataset = None
+        self.connection = psycopg2.connect(host=self.host, port=self.port, dbname=self.database, user=self.username, password=self.password)
+        self.connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+        self.cursor = self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     def try_connect(self):
         try:
@@ -58,13 +57,14 @@ class DbServer():
     def __str__(self):
         vars=dict(self.db_conn.__dict__)
         vars.pop('connection')
+        vars.pop('cursor')
         vars.pop('password')
         vars.pop('dataset')
         return '''{"name": "''' + self.name  + '''", "db_conn":''' + json.dumps(vars) + '}'
 
 
 if __name__ == '__main__':
-    dbc = DBConnection('localhost', 5432, 'postgres', 'kmoppel')
+    dbc = DBConnection('localhost', 5432, 'postgres', 'kmoppel', '')
     dbs = DbServer('srv1', dbc)
     # print dbc.try_connect()
     print dbs
